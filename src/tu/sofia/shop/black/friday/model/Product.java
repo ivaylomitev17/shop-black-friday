b2daa4fab54528e3cddb7769ee74c4aee2a71d75
ivaylomitev17 <ivailomitev17@gmail.com>
2020-01-03 14:48:58 +0200

    >Product services moved to ProductService.java >Deleted UserRegistration and UserLogin classes and added their functionalities into new UserService.java >Order.java moved to service package

--- src/tu/sofia/shop/black/friday/model/Product.java
+++ src/tu/sofia/shop/black/friday/model/Product.java
@@ -1,6 +1,6 @@
 package tu.sofia.shop.black.friday.model;
 
-import tu.sofia.shop.black.friday.util.ConnectionToDB;
+import tu.sofia.shop.black.friday.util.DatabaseControlUnit;
 
 import java.sql.ResultSet;
 import java.sql.Statement;
@@ -25,60 +25,6 @@ public class Product {
     public Product(){
 
     }
-
-    public void addProductToDB (){
-        System.out.println("Input Product name:");
-        setName(sc.nextLine());
-        System.out.println("Input Actual Price:");
-        setPrice(Double.parseDouble(sc.nextLine()));
-        System.out.println("Input Minimum price");
-        setPriceMin(Double.parseDouble(sc.nextLine()));
-        System.out.println("Input quantity");
-        setQuantity(Integer.parseInt(sc.nextLine()));
-        String query = "insert into products(name, price,priceMin,quantity,status,blackFridayDiscountPercentage) values ('" + this.name + "','" + this.price + "','" + this.priceMin+ "','" +this.quantity+ "','BlackFridayOFF','0')";
-
-        try{
-            ConnectionToDB connection = new ConnectionToDB();
-            connection.createConnection();
-            Statement statement = connection.getConnection().createStatement();
-            statement.executeUpdate(query);
-            connection.getConnection().close();
-        }catch (Exception e){
-            System.out.println("A problem occurred connecting to database");
-            e.printStackTrace();
-        }
-
-    }
-    public ResultSet searchProductByName()throws Exception{
-        System.out.println("Input product name for search:");
-        String query = "SELECT * FROM products WHERE name = '"+sc.nextLine()+ "'";
-            ConnectionToDB connection = new ConnectionToDB();
-            connection.createConnection();
-            Statement statement = connection.getConnection().createStatement();
-            ResultSet resultSet = statement.executeQuery(query);
-            resultSet.first();
-        //System.out.println(resultSet.getString(2));
-        return resultSet;
-    }
-
-
-    public void changeProductPrice  () throws Exception{
-        ResultSet resultSet = searchProductByName();
-        //int id = Integer.parseInt(resultSet.getString (1))
-        System.out.println("Current product price:"+resultSet.getString(3)+"    (minimum price:)"+resultSet.getString(4));
-        System.out.println("Input new price:");
-        double newPrice = Double.parseDouble(sc.nextLine());
-        if ((Double.compare(newPrice,Double.parseDouble(resultSet.getString(4))))>0){
-            ConnectionToDB connection = new ConnectionToDB();
-            connection.updateDB("products","price",Double.toString(newPrice),"id",resultSet.getString(1));
-        }
-        else System.out.println("Price cannot be lower than minimum price!");
-    }
-
-
-
-
-
 
 
 
--- src/tu/sofia/shop/black/friday/service/ProductService.java
+++ src/tu/sofia/shop/black/friday/service/ProductService.java
@@ -1,25 +1,49 @@
 package tu.sofia.shop.black.friday.service;
 
 import tu.sofia.shop.black.friday.model.Product;
+import tu.sofia.shop.black.friday.util.DatabaseControlUnit;
+import tu.sofia.shop.black.friday.util.DatabaseManipulation;
+
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Scanner;
 
 public class ProductService {
 
     // Note: methods are dummy, add the needed parameters and implement the functionality
+    public void create(Product productToAddToDatabase) {
+        String query = "insert into products(name, price,priceMin,quantity,status,blackFridayDiscountPercentage)" +
+                " values ('" + productToAddToDatabase.getName() + "','" + productToAddToDatabase.getPrice() + "','" +
+                productToAddToDatabase.getPriceMin()+ "','" +productToAddToDatabase.getQuantity()+ "','BlackFridayOFF','0')";
 
-    public Product create() {
-        // TODO: creation of a new product in database
+        try{
+            DatabaseManipulation insertStatement = new DatabaseManipulation();
+            insertStatement.insertIntoDatabase(query);
+        }catch (Exception e){
+            System.out.println("A problem occurred connecting to database");
+            e.printStackTrace();
+        }
 
-        return null; // TODO: return the created product
     }
 
-    public Product findProductByName() {
-        // TODO: search for product
-
-        return null; // TODO: return the product
+    public ResultSet findProductByName(String name) throws Exception{
+        String query = "SELECT * FROM products WHERE name = '"+name+ "'";
+        DatabaseManipulation selectQuery = new DatabaseManipulation();
+        ResultSet resultSet = selectQuery.createStatement().executeQuery(query);
+        resultSet.first();
+        return resultSet;
     }
 
-    public Product updateProductPrice(){
+    public Product updateProductPrice(String name, double newPrice)throws Exception{
         // TODO: update the product price
+        ResultSet resultSet = findProductByName(name);
+        //System.out.println("Current product price:"+resultSet.getString(3)+"    (minimum price:)"+resultSet.getString(4));
+        //double newPrice = Double.parseDouble(sc.nextLine());
+        if ((Double.compare(newPrice,Double.parseDouble(resultSet.getString(4))))>0){
+            DatabaseManipulation updateStatement = new DatabaseManipulation();
+            updateStatement.updateDatabase("products","price",Double.toString(newPrice),"id",resultSet.getString(1));
+        }
+        else System.out.println("Price cannot be lower than minimum price!");
 
         return null; // return the updated product
     }
